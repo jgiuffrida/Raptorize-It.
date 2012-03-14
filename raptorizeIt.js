3df5043e27300bc6522a699b1d7f86a43adcbb19
Joseph Giuffrida <josephgiuffrida@Josephs-MacBook-Air.local>
2012-03-13 22:40:02 -0400

    Initial Import

---
+++ raptorizeIt.js
@@ -0,0 +1,6 @@
+(function(){
+	var a=document.createElement('script');
+	a.type='text/javascript';
+	a.src='http://www.josephgiuffrida.com/raptor/raptor.js';
+	document.getElementsByTagName('body')[0].appendChild(a);
+})();
